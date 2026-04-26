8cb1c55f6cc7841939c8c5ad10b91de5df75f3b7
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2026-04-26 22:12:43 +0530

    Add: populating-next-right-pointers-in-each-node - submission-0

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-0.java
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> qu = new ArrayDeque<>();
+        if(root==null) return root;
+        qu.offer(root);
+
+        while(!qu.isEmpty()){
+            int levelNum = qu.size();
+            Node prev = null;
+            for(int i=0; i<levelNum; i++){
+                if(qu.peek().left != null) qu.offer(qu.peek().left);
+                if(qu.peek().right != null) qu.offer(qu.peek().right);
+
+                if(prev != null) prev.next = qu.peek();
+                prev = qu.peek();
+                qu.poll();
+            }
+        }
+
+        return root;
+    }
+}
